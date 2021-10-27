201d455a4cc976f7f37f1f5c63b3eaab435093ce
mikaeldiok <60207273+mikaeldiok@users.noreply.github.com>
2021-10-27 19:15:01 +0700

    Initial commit

---
+++ resources/views/auth/footer.blade.php
@@ -0,0 +1,18 @@
+<footer class="py-5 bg-white" id="footer-main">
+    <div class="container">
+        <div class="row align-items-center justify-content-xl-between">
+            <div class="col-xl-6">
+                <div class="copyright text-center text-xl-left text-muted">
+                    &copy; {{ config('app.name') }}, {!! setting('footer_text') !!}
+                </div>
+            </div>
+            <div class="col-xl-6">
+                <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">License</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
